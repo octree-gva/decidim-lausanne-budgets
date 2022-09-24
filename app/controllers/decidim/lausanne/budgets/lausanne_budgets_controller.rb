76b2127bf15e9ae07c65d69bedfcb9929636e90f
Hadrien Froger <hadrien@octree.ch>
2022-09-24 09:16:22 -0300

    refresh on budgets page

--- app/controllers/decidim/lausanne/budgets/lausanne_budgets_controller.rb
+++ app/controllers/decidim/lausanne/budgets/lausanne_budgets_controller.rb
@@ -5,6 +5,8 @@ module Decidim
     module Budgets
       # Exposes the budget resources so users can participate on them
       class LausanneBudgetsController < Decidim::Lausanne::Budgets::ApplicationController
+        before_filter :set_cache_headers
+
         def index
           redirect_to lausanne_budget_projects_path(current_workflow.single) if current_workflow.single?
         end
@@ -14,8 +16,14 @@ module Decidim
           redirect_to lausanne_budget_projects_path(budget)
         end
 
+          
         private
 
+          def set_cache_headers
+            response.headers["Cache-Control"] = "no-cache, no-store, max-age=0, must-revalidate"
+            response.headers["Pragma"] = "no-cache"
+            response.headers["Expires"] = "Fri, 01 Jan 1990 00:00:00 GMT"
+          end
           def budget
             @budget ||= LausanneBudget.where(component: current_component).includes(:projects).find_by(id: params[:id])
           end
